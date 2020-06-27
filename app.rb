24989a5413d484a1191029ca825802979daea227
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-27 17:48:10 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,14 +1,8 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-
-  get '/food_form' do
-    erb :food_form
-  end
-
+  
   # Add your post route and action below
-  <form method="POST" action="/food">
-
   post '/food' do
     "My name is #{params[:name]}, and I love #{params[:favorite_food]}."
   end
